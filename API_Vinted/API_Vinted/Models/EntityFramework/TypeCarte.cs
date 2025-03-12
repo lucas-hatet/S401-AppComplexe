6bb79019b2a096bdc53d526870fa6d46f3cb556b
Tanguy ABDOULVAID <abdoulvt@iut-acy.local>
2025-03-12 10:40:02 +0100

    ajout typecarte

--- API_Vinted/API_Vinted/Models/EntityFramework/TypeCarte.cs
+++ API_Vinted/API_Vinted/Models/EntityFramework/TypeCarte.cs
@@ -1,6 +1,17 @@
-﻿namespace API_Vinted.Models.EntityFramework
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_Vinted.Models.EntityFramework
 {
+    [Table("typecarte")]
     public class TypeCarte
     {
+        [Key]
+        [Column("idtypecarte")]
+        public int IDTypeCarte { get; set; }
+
+        [Column("libelletypecarte")]
+        [StringLength(50)]
+        public String LibelleTypeCarte { get; set; } = null!;
     }
 }
