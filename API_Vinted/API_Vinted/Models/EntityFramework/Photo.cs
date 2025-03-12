9a8dc80c0a4964f52a081efa8d32f11f78fdc152
Tanguy ABDOULVAID <abdoulvt@iut-acy.local>
2025-03-12 15:19:22 +0100

    ajout photo

--- API_Vinted/API_Vinted/Models/EntityFramework/Photo.cs
+++ API_Vinted/API_Vinted/Models/EntityFramework/Photo.cs
@@ -1,10 +1,26 @@
+<<<<<<< Updated upstream
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+=======
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+>>>>>>> Stashed changes
 
 namespace API_Vinted.Models.EntityFramework
 {
+    [Table("photo")]
     public class Photo
     {
+<<<<<<< Updated upstream
         [InverseProperty(nameof(Client.Photo))]
         public List<Client> Clients { get; set; } = null!;
+=======
+        [Key]
+        [Column("idphoto")]
+        public int IDPhoto { get; set; }
+
+        [Required]
+        [Column("urlphoto")]
+        public string URLPhoto { get; set; } = null!;
+>>>>>>> Stashed changes
     }
 }
