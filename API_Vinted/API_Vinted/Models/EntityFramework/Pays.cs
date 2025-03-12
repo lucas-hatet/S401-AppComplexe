aeac9d4174e71399b5289681f00324562a8249f6
Tanguy ABDOULVAID <abdoulvt@iut-acy.local>
2025-03-12 15:24:40 +0100

    ajout model pays

--- API_Vinted/API_Vinted/Models/EntityFramework/Pays.cs
+++ API_Vinted/API_Vinted/Models/EntityFramework/Pays.cs
@@ -1,9 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API_Vinted.Models.EntityFramework
 {
+    [Table("Pays")]
     public class Pays
     {
+        [Key]
+        [Column("idpays")]
+        public int IDPays { get; set; }
+
+        [Required]
+        [Column("nompays")]
+        public string NomPays { get; set; } = null!;
+
         [InverseProperty(nameof(Ville.Pays))]
         public virtual ICollection<Ville> Villes { get; set; } = null!;
     }
