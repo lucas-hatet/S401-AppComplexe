8479df934feeb738659441fbe9662ed182ad73c8
Tanguy ABDOULVAID <abdoulvt@iut-acy.local>
2025-03-12 15:22:10 +0100

    ajout photo

--- API_Vinted/API_Vinted/Models/EntityFramework/Photo.cs
+++ API_Vinted/API_Vinted/Models/EntityFramework/Photo.cs
@@ -1,35 +1,18 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 ﻿using System.ComponentModel.DataAnnotations.Schema;
-=======
+
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
->>>>>>> Stashed changes
-=======
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
->>>>>>> Stashed changes
-=======
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
->>>>>>> Stashed changes
+
 
 namespace API_Vinted.Models.EntityFramework
 {
     [Table("photo")]
     public class Photo
     {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
         [InverseProperty(nameof(Client.Photo))]
         public List<Client> Clients { get; set; } = null!;
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
         [Key]
         [Column("idphoto")]
         public int IDPhoto { get; set; }
@@ -37,12 +20,6 @@ namespace API_Vinted.Models.EntityFramework
         [Required]
         [Column("urlphoto")]
         public string URLPhoto { get; set; } = null!;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
     }
 }
